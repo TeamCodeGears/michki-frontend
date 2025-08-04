febfcb5b4bab4d70547a1130df9b663c06e74d63
normalization00 <dlwpgks1@gmail.com>
2025-08-04 17:48:58 +0900

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // 이 부분을 추가합니다. 저장소 이름으로 바꿔주세요.
-  base: "/michiki-project-frontend/", 
-})
