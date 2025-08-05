b2e3aacce6ceae8c82501788f9900a6c57c992c1
normalization00 <dlwpgks1@gmail.com>
2025-08-05 16:20:45 +0900

    중복된 src삭제

--- src/src/utils/toLatLngObj.js
+++
@@ -1,7 +0,0 @@
-export default function toLatLngObj(pos) {
-  if (!pos) return null;
-  if (typeof pos.lat === "function" && typeof pos.lng === "function") {
-    return { lat: pos.lat(), lng: pos.lng() };
-  }
-  return pos;
-}
